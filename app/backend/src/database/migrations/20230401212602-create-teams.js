8908ebff27c17eb364d1350e55d45cb4d237199b
Victor Hugo <victorhdoliveira@gmail.com>
2023-04-01 18:54:47 -0300

    feat: teams model and migration

---
+++ app/backend/src/database/migrations/20230401212602-create-teams.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams',{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      teamName: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams');
+  }
+};
