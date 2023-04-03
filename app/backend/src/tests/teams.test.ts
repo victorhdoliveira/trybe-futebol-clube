805f88e2b0b66b74f9a5ebc3c0b990f6f0de019b
Victor Hugo <victorhdoliveira@gmail.com>
2023-04-03 15:41:17 -0300

    fix: teams test

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -12,22 +12,25 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Varifica a rota /teams', async () => {
+describe('Verifica a rota /teams', async () => {
     let chaiHttpResponse: Response
-
-  it('Busca o time conforme o id', async  () => {
-    sinon.stub(Teams, 'findOne').resolves(teamById as Teams);
-
-    chaiHttpResponse = await chai.request(app).get('/teams/1');
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.equal(teamById);
-  });
+    afterEach(sinon.restore);
 
   it('Busca a lista de todos os times', async  () => {
     sinon.stub(Teams, 'findAll').resolves(allTeams as Teams[]);
-
     chaiHttpResponse = await chai.request(app).get('/teams');
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.equal(allTeams);
+    const {status, body} = chaiHttpResponse;
+    
+    expect(status).to.be.eq(200)
+    expect(body).to.be.deep.eq(allTeams);
+  });
+
+  it('Busca o time conforme o id', async  () => {
+    sinon.stub(Teams, 'findByPk').resolves(teamById as Teams);
+    chaiHttpResponse = await chai.request(app).get('/teams/1');
+    const {status, body} = chaiHttpResponse;
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.eq(teamById);
   });
 });
