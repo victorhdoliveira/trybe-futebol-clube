c1e19b8b9f4220e99b62e28277662838ed9e4022
Victor Hugo <victorhdoliveira@gmail.com>
2023-04-04 17:16:08 -0300

    feat: email and password validation

--- app/backend/src/database/middleware/verifyLogin.ts
+++ app/backend/src/database/middleware/verifyLogin.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 
 const loginValidation = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
+  const regex = /\S+@\S+\.\S+/;
 
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  if (!regex.test(email) || password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   return next();
 };
